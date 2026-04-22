0adfd581340cc1243f818a826bcbcf348d5fc68b
Anil Krishna <anil.kumar.konasale.krishna@alaskaair.com>
2026-04-21 20:52:35 -0700

    App Insights

--- API/TestASApi/AirTravelAccomodations/Controllers/EnvironmentController.cs
+++ API/TestASApi/AirTravelAccomodations/Controllers/EnvironmentController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace AirTravelAccomodations.Controllers
@@ -8,9 +10,17 @@ namespace AirTravelAccomodations.Controllers
     [Route("api/[controller]")]
     public class EnvironmentController : ControllerBase
     {
+        private readonly ILogger<EnvironmentController> _logger;
+
+        public EnvironmentController(ILogger<EnvironmentController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("check-env")]
         public ActionResult<object> CheckAppInsightsConnectionString()
         {
+            _logger.LogInformation("Checking Application Insights connection string. Correlation ID: {correlationId}", Activity.Current?.Id);
             var connectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
             bool isSet = !string.IsNullOrEmpty(connectionString);
             return Ok(new { isApplicationInsightsConfigured = isSet });
@@ -19,6 +29,7 @@ namespace AirTravelAccomodations.Controllers
         [HttpGet("check-appconfig")]
         public ActionResult<object> CheckAppConfigEndpoint()
         {
+            _logger.LogInformation("Checking App Config endpoint. Correlation ID: {correlationId}", Activity.Current?.Id);
             var appConfigEndpoint = Environment.GetEnvironmentVariable("AZURE_Travel_APPCONFIG");
             bool isSet = !string.IsNullOrEmpty(appConfigEndpoint);
             return Ok(new { isAppConfigConfigured = isSet });
@@ -27,6 +38,7 @@ namespace AirTravelAccomodations.Controllers
         [HttpGet("check-all-env")]
         public ActionResult<object> CheckAllEnvironmentVariables()
         {
+            _logger.LogInformation("Checking all environment variables. Correlation ID: {correlationId}", Activity.Current?.Id);
             var appInsightsConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
             var appConfigEndpoint = Environment.GetEnvironmentVariable("AZURE_Travel_APPCONFIG");
             var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
